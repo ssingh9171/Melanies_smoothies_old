e2e8e7f77bbc746863c42085ca3351c1f9f4130a
ssingh9171 <sunderrajpoot147@gmail.com>
2024-05-21 18:28:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import snowflake.connector
 
 # Write directly to the app
 st.title(":cup_with_straw:  Customise your smoothie :cup_with_straw:")
